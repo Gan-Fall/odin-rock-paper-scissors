fefeb176cafcb212158f8f38e6490209fa38e58f
Gan-Fall <98006378+Gan-Fall@users.noreply.github.com>
2026-04-19 19:27:40 -0400

    Add getComputerChoice function
    This function returns the computer's choice in the rock-paper-scissors
    game as a string of the appropriate choice (all lower-case).

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    let guess = Math.floor(Math.random() * 100) + 1;
+    if (guess > 66) {
+        return "scissors"
+    } else if (guess < 33) {
+        return "rock"
+    } else {
+        return "paper"
+    }
+}
