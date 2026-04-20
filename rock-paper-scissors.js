75d52e3472ab000192751af5a1f9edb4d2bc2f42
Gan-Fall <98006378+Gan-Fall@users.noreply.github.com>
2026-04-19 20:17:47 -0400

    Format rock-paper-scissors.js a little bit
    Add whitespace to help with readability a little bit. As well as add
    comments to explain why some seemingly bad choices were made. A more
    "proper" implementation would add input sanitation, not immediately
    convert user input to lowercase, and/or have a more rigorous playRound
    logic with error checking.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,13 +2,19 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-    let guess = Math.floor(Math.random() * 100) + 1;
+    let guess = Math.floor(Math.random() * 100) + 1; // Will return a number between 1-100
     if (guess > 66) {
+
         return "scissors"
+
     } else if (guess < 33) {
+
         return "rock"
+
     } else {
+
         return "paper"
+
     }
 }
 
@@ -17,14 +23,24 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
+
     if (humanChoice === computerChoice) {
+
         console.log(`Tie! Player and COM both chose ${humanChoice}.`);
+
     } else if (humanChoice === "rock" && computerChoice === "paper"
             || humanChoice === "paper" && computerChoice === "scissors"
             || humanChoice === "scissors" && computerChoice === "rock") {
+
         console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
         computerScore += 1;
+
     } else {
+        // This logic relies heavily on playRound having a STRICT function
+        // domain of these 3 strings: "rock", "paper", "scissors".
+        // If user is winning with whacky inputs like "gun" this is why.
+        // I chose to forego input sanitation or extra checks here as
+        // the assignment doesn't really require it.
         console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
         humanScore += 1;
     }
