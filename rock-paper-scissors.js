ac6a5004d2edbe0b5db017f7649a171a2388933f
Gan-Fall <98006378+Gan-Fall@users.noreply.github.com>
2026-05-06 16:27:25 -0400

    Remove 5 round game logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -54,10 +54,6 @@ function playGame() {
         }
     }
 
-    for (let i = 0; i <= 4; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
     if (humanScore > computerScore) {
 
         console.log(`You win! Final score:
