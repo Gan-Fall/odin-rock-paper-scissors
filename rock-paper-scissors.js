0095bd3557eef43e053248cd41936305796bc8cf
Gan-Fall <98006378+Gan-Fall@users.noreply.github.com>
2026-05-06 17:46:02 -0400

    Refactor rock-paper-scissors.js
    -Human and COM score are now global variables
    -No longer use playGame function as a pseudo main
      -Game now plays a round whenever user clicks a button
      -Score is checked after round for a winner (5+ points)
      -Instead of main game logic being in a for loop, its on the
      button click callback events
    -Added check score function to check player and pc scores
    -Alerts player when one player reaches 5 points, instead of logging to
    console

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -21,58 +21,69 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    return prompt("Rock Paper Scissors! Choose your play:").toLowerCase()
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice === computerChoice) {
+
+        console.log(`Tie! Player and COM both chose ${capitalize(humanChoice)}.`);
+
+    } else if (humanChoice === "rock" && computerChoice === "paper"
+        || humanChoice === "paper" && computerChoice === "scissors"
+        || humanChoice === "scissors" && computerChoice === "rock") {
+
+        console.log(`You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}.`);
+        computerScore += 1;
+
+    } else {
+        // This logic relies heavily on playRound having a STRICT function
+        // domain of these 3 strings: "rock", "paper", "scissors".
+        // If user is winning with whacky inputs like "gun" this is why.
+        // I chose to forego input sanitation or extra checks here as
+        // the assignment doesn't really require it.
+        console.log(`You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}.`);
+        humanScore += 1;
+    }
 }
 
-function playGame() {
-
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
-
-        if (humanChoice === computerChoice) {
-
-            console.log(`Tie! Player and COM both chose ${capitalize(humanChoice)}.`);
-
-        } else if (humanChoice === "rock" && computerChoice === "paper"
-            || humanChoice === "paper" && computerChoice === "scissors"
-            || humanChoice === "scissors" && computerChoice === "rock") {
-
-            console.log(`You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}.`);
-            computerScore += 1;
-
-        } else {
-            // This logic relies heavily on playRound having a STRICT function
-            // domain of these 3 strings: "rock", "paper", "scissors".
-            // If user is winning with whacky inputs like "gun" this is why.
-            // I chose to forego input sanitation or extra checks here as
-            // the assignment doesn't really require it.
-            console.log(`You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}.`);
-            humanScore += 1;
-        }
-    }
-
+function checkScore() {
+    if (humanScore < 5 && computerScore < 5) return
     if (humanScore > computerScore) {
 
-        console.log(`You win! Final score:
+        alert(`You win! Final score:
             Human: ${humanScore}
             COM: ${computerScore}`);
 
     } else if (computerScore > humanScore) {
 
-        console.log(`You lose! Final score:
+        alert(`You lose! Final score:
             Human: ${humanScore}
             COM: ${computerScore}`);
 
     } else {
 
-        console.log(`Tie! Final score:
+        alert(`Tie! Final score:
             Human: ${humanScore}
             COM: ${computerScore}`);
 
     }
 }
 
-playGame();
+let humanScore = 0;
+let computerScore = 0;
+
+let btns = document.querySelector("#Choices");
+btns.addEventListener("click", (event) => {
+    let target = event.target;
+    switch (target.id) {
+        case 'Rock':
+            playRound("rock", getComputerChoice());
+            break;
+        case 'Paper':
+            playRound("paper", getComputerChoice());
+            break;
+        case 'Scissors':
+            playRound("scissors", getComputerChoice());
+            break;
+    }
+    checkScore();
+});
